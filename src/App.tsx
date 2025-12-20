33b1692f9bdcd77c38fdcb486863aeceadbaa3c6
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-12-20 18:28:05 -0500

    starter scss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div data-text-preset="body">
+      <div data-text-preset="heading-1">Heading 1</div>
+      <div data-text-preset="heading-2">Heading 2</div>
+      <div data-text-preset="heading-3">Heading 3</div>
+      <div data-text-preset="title-value">Title value</div>
+      <div data-text-preset="title-step">Title step</div>
+      <div data-text-preset="title-card">Title step</div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        officiis similique eaque eveniet accusantium esse eligendi! Sunt
+        necessitatibus, harum facere itaque, fuga ea distinctio doloremque
+        similique, officiis quos placeat?
+      </p>
+    </div>
+  );
 }
