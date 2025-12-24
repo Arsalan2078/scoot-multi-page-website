c516235df122201f225da2225f08a1abe3b6af24
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-12-23 20:45:14 -0500

    footer complete

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,16 @@
+import AppStoreLinks from "../AppStoreLinks";
 import NavbarBottom from "../NavbarBottom";
 import css from "./index.module.scss";
 
 export default function Footer() {
   return (
     <footer className={`${css.footer}`}>
-      <div></div>
+      <div className={`${css.content}`}>
+        <div className={`${css.container} container`}>
+          <h2 data-text-preset="heading-2">Sign up and<br className={`${css.br}`} /> Scoot off today</h2>
+          <AppStoreLinks />
+        </div>
+      </div>
 
       <NavbarBottom />
     </footer>
