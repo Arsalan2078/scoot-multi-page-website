1bb797e4485087885b2a26e57a6bcc263bd0ab1e
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-12-23 19:21:25 -0500

    added container to footer navbar

--- src/components/NavbarBottom/index.tsx
+++ src/components/NavbarBottom/index.tsx
@@ -6,11 +6,13 @@ import css from "./index.module.scss";
 export default function NavbarBottom() {
   return (
     <nav className={`${css.nav}`}>
-      <LogoLink className={`${css.logo}`}/>
+      <div className={`${css.container} container`}>
+        <LogoLink className={`${css.logo}`} />
 
-      <div className={`${css.stack}`}>
-        <NavLinks className={css.links} />
-        <SocMeds />
+        <div className={`${css.stack}`}>
+          <NavLinks className={css.links} />
+          <SocMeds />
+        </div>
       </div>
     </nav>
   );
